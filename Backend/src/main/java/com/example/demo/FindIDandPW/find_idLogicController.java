06650cd70687a7ac3817856ed4b826c7d2c204cd
kkj7584 <kkj758440@gmail.com>
2025-05-19 02:28:47 +0900

    아이디 찾기 로직 새로 작성

--- Backend/src/main/java/com/example/demo/FindIDandPW/find_idLogicController.java
+++ Backend/src/main/java/com/example/demo/FindIDandPW/find_idLogicController.java
@@ -1,45 +1,44 @@
 package com.example.demo.FindIDandPW;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.User.DAOUser;
 import com.example.demo.User.User;
 
-@Controller
+@RestController
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")/* 프론트주소 바뀌면 같이 바꾸어 줘어야 함. */
 public class find_idLogicController {
     
     @Autowired
     DAOUser daoUser;
 
     @PostMapping("/find_id/logic")
-    public String find_id_logic(/* ID 찾기 로직 작성 */
-        @RequestParam("name") String name,
-        @RequestParam("email") String email,
-        @RequestParam("phone_number") String phone,
-        @RequestParam("method") String method,
-        Model model
-    ){
+    public Map<String, String> find_id_logic(@RequestBody Map<String, String> data)/* ID 찾기 로직 작성 */
+    {
+        String name = data.get("name");
+        String email = data.get("email");
+        String phone = data.get("phone_number");
+        String method = data.get("method");
+
         User user=null;
 
         if(method.equals("email"))/* 이메일을 활용한 찾기 */
-            user=daoUser.findNameEmail(name, email);
+            user=daoUser.findNameEmail(name,email);
         else if(method.equals("phone_number"))/* 휴대폰 번호를 활용한 찾기 */
-            user=daoUser.findNamePhone(name, phone);
+            user=daoUser.findNamePhone(name,phone);
 
-        if(user==null){/* 해당하는 정보가 DB에 없을 경우 */
-            model.addAttribute("foundID",false);
-            model.addAttribute("noInfo",true);/* 해당하는 정보가 존재하지 않는다는 텍스트를 띄우기 위한 변수 설정 */
-        }
-        else{
-            model.addAttribute("foundID",true);
-            model.addAttribute("resultID",user.getUsername().substring(0,user.getUsername().length()-3)+"***");/* 아이디의 뒷 세 자리 부분만 빼고 표현 */
-        }
+        Map<String, String> response = new HashMap<>();
+        if(user==null)response.put("foundID", "");
+        else response.put("foundID",user.getUsername().substring(0, user.getUsername().length() - 3)+"***");
 
-        return "find_id";
+        return response;
     }
 
 }
