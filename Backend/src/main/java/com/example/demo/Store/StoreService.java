c470f8db3904e99f6cd02f07cf41bc366c3589e8
bum <mms7080@naver.com>
2025-05-21 02:30:23 +0900

    스토어 백엔드

--- Backend/src/main/java/com/example/demo/Store/StoreService.java
+++ Backend/src/main/java/com/example/demo/Store/StoreService.java
@@ -38,12 +38,15 @@ public class StoreService {
     public void initDummyStoreItems() {
         if (repo.count() == 0) {
             List<Store> items = List.of(
-                new Store(null, "메가티켓", "일반관람권", "일반 관람권", "13,000원", null, "대표상품", "black", "https://www.megabox.co.kr/static/pc/images/store/img-product-ticket1.png"),
-                new Store(null, "메가티켓", "Dolby Cinema 전용관람권", "Dolby Cinema 전용관람권", "18,000원", null, "추천", "#1e88e5", "https://www.megabox.co.kr/static/pc/images/store/img-product-ticket-dolby.png"),
-                new Store(null, "메가티켓", "더 부티크 관람권", "더 부티크 전용 관람권", "15,000원", "16,000원", null, null, "https://www.megabox.co.kr/static/pc/images/store/img-product-ticket-boutique.png"),
-                new Store(null, "메가티켓", "더 부티크 스위트 관람권", "더 부티크 스위트 전용관람권", "40,000원", null, "추천", "#1e88e5", "https://www.megabox.co.kr/static/pc/images/store/img-product-ticket-suite.png")
+                new Store(null, "티켓", "일반관람권", "일반 관람권", "13,000원", null, "대표상품", "black", "/images/ticket.png"),
+                new Store(null, "티켓", "Dolby Cinema 전용관람권", "Dolby Cinema 전용관람권", "18,000원", null, "추천", "#1e88e5", "/images/ticket.png"),
+                new Store(null, "티켓", "더 부티크 관람권", "더 부티크 전용 관람권", "15,000원", "16,000원", null, null, "/images/ticket.png"),
+                new Store(null, "티켓", "더 부티크 스위트 관람권", "더 부티크 스위트 전용관람권", "40,000원", null, "추천", "#1e88e5", "/images/ticket.png"),
+                new Store(null, "팝콘/음료/콤보", "더블콤보", "팝콘(R) 2 + 탄산음료(R) 2 ", "13.900원", null, "추천", "#1e88e5", "/images/corn.png"),
+                new Store(null, "팝콘/음료/콤보", "러브콤보", "팝콘(L) 1 + 탄산음료(R) 2 ", "10.900원", null, "추천", "#1e88e5", "/images/lovecorn.png")
             );
             repo.saveAll(items);
         }
     }
+
 }
