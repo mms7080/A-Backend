ad5949bc926920735a3d6ae8caba1582e9f29746
bum <mms7080@naver.com>
2025-05-21 06:50:27 +0900

    스토어 삭제기능

--- Backend/src/main/java/com/example/demo/Store/StoreController.java
+++ Backend/src/main/java/com/example/demo/Store/StoreController.java
@@ -21,21 +21,26 @@ public class StoreController {
         this.service = service;
     }
 
+    //  카테고리별 상품 리스트 반환
     @GetMapping
     public Map<String, List<Store>> getGroupedItems() {
         return service.getGroupedByCategory();
     }
 
+    // 상품 상세 조회
     @GetMapping("/detail/{id}")
     public Store getItemById(@PathVariable Long id) {
         return service.findById(id);
     }
 
+    //  상품 삭제
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id) {
         service.delete(id);
     }
 
+    //  상품 업로드 (이미지 포함)
     @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     public Store uploadItem(
