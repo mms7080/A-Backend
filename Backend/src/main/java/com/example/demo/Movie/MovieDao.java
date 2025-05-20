527bcd062c3b7490975d143e13dc10779bbe6149
ParkChaeHun <pch3482@naver.com>
2025-05-20 18:07:55 +0900

    영화DB 전체 영화 페이지랑 연결

--- Backend/src/main/java/com/example/demo/Movie/MovieDao.java
+++ Backend/src/main/java/com/example/demo/Movie/MovieDao.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import jakarta.annotation.PostConstruct;
 
 @Component
 public class MovieDao {
@@ -16,5 +19,19 @@ public class MovieDao {
 
     public void save(Movie movie) {
         movieRep.save(movie);
+    }
+
+    @PostConstruct
+    @Transactional
+    public void insertTestMovies() {
+        if(movieRep.count() > 0) return;
+        save(new Movie(null, "Fields of Destiny", "", "0000.00.00", 5300L, "https://res.cloudinary.com/upwork-cloud/image/upload/c_scale,w_1000/v1700795880/catalog/1600659718750367744/xiry6ufbjttckqxpfzrw.jpg"));
+        save(new Movie(null, "Killer Advice", "", "0000.00.00", 2100L, "https://res.cloudinary.com/upwork-cloud/image/upload/c_scale,w_1000/v1700796426/catalog/1600659718750367744/iqmiudmmo6s7zcofwmpf.jpg"));
+        save(new Movie(null, "InterStella", "12", "2014.11.06", 1500L, "https://rukminim2.flixcart.com/image/850/1000/l2dmky80/poster/y/f/b/small-poster-interstellar-sl407-wall-poster-13x19-inches-matte-original-imagdqezkfchjkhz.jpeg?q=20&crop=false"));
+        save(new Movie(null, "My Name is Alfred Hitchcock", "", "0000.00.00", 1300L, "https://www.mvtimes.com/mvt/uploads/2024/11/film-my-name-alfred-hitchcock-2.jpg"));
+        save(new Movie(null, "어벤져스 엔드게임", "12", "2019.04.24", 986L, "https://upload.wikimedia.org/wikipedia/ko/thumb/f/f2/%EC%96%B4%EB%B2%A4%EC%A0%B8%EC%8A%A4-_%EC%97%94%EB%93%9C%EA%B2%8C%EC%9E%84_%ED%8F%AC%EC%8A%A4%ED%84%B0.jpg/1200px-%EC%96%B4%EB%B2%A4%EC%A0%B8%EC%8A%A4-_%EC%97%94%EB%93%9C%EA%B2%8C%EC%9E%84_%ED%8F%AC%EC%8A%A4%ED%84%B0.jpg"));
+        save(new Movie(null, "범죄도시 4", "15", "2024.04.24", 734L, "https://i.namu.wiki/i/KwJ2dfIySu2k8JWlK3nD-gS7A9G-2I2EWKkNjoVRqaHabjK88STUo8FXi545XV6Pe8ERSX5DjF4e5k0IkOvznQ.webp"));
+        save(new Movie(null, "귀멸의칼날 무한성편", "19", "2025.08.22", 521L, "https://i.namu.wiki/i/YvPBZ1kzk8Dku4HhOC2FGB7xKVXj5bpg8cSdRWsAZg-3Knqu5LcWJchrZDIVmz-08V3OV9uFLMfCRNCZRcnTxQ.webp"));
+        save(new Movie(null, "승부", "12", "2025.03.26", 342L, "https://img.megabox.co.kr/SharedImg/2025/03/27/O6RnDMOAnUw6geDdlaAXRlkqgy0mSSDb_420.jpg"));
     }
 }
