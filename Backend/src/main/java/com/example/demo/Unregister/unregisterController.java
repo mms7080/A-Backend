9c222fec10828ef8ebe59974443e6299316f9899
kkj7584 <kkj758440@gmail.com>
2025-05-21 21:49:29 +0900

    회원탈퇴 로직 작성

--- Backend/src/main/java/com/example/demo/Unregister/unregisterController.java
+++ Backend/src/main/java/com/example/demo/Unregister/unregisterController.java
@@ -2,7 +2,6 @@ package com.example.demo.Unregister;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.demo.Annotations.Auth;
@@ -21,11 +20,10 @@ public class unregisterController {
     DAOUser daoUser;
 
     @GetMapping("/unregister")
-    public String unregister(HttpServletResponse response,@Auth User user,Model model){
+    public String unregister(HttpServletResponse response,@Auth User user){
         daoUser.Delete(user);/* 현재 접속중인 회원의 탈퇴 */
         cookieUtil.RemoveJWTCookie(response);/* JWT 쿠키 제거 */
-        model.addAttribute("isLogin",false);/* 회원탈퇴 직후에는 로그인 상태가 아니라는 것을 표현하기 위해 isLogin에 false 대입 */
-        return "home";/* 홈 화면으로 이동 */
+        return "redirect:http://localhost:3000/home";/* 홈 화면으로 이동 */
     }    
 
 }
