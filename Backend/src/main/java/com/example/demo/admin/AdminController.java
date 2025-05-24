092a188e563cc715a1a9eb3ca1f0b0e4961810c1
bum <mms7080@naver.com>
2025-05-25 02:55:38 +0900

    관리자페이지

--- Backend/src/main/java/com/example/demo/admin/AdminController.java
+++ Backend/src/main/java/com/example/demo/admin/AdminController.java
@@ -1,10 +1,15 @@
 package com.example.demo.admin;
 
 import com.example.demo.User.UserRepository;
+import com.example.demo.Event.Event;
+import com.example.demo.Event.EventRepository;
+import com.example.demo.Movie.Movie;
+import com.example.demo.Movie.MovieRepository;
 import com.example.demo.Store.StoreRepository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,16 +17,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 @RestController
-@RequestMapping("/admin") 
+@RequestMapping("/admin")
 public class AdminController {
-
 
     private final UserRepository userRepository;
     private final StoreRepository storeRepository;
+    private final MovieRepository movieRepository;
+    private final EventRepository eventRepository;
 
-    public AdminController(UserRepository userRepository, StoreRepository storeRepository) {
+    public AdminController(UserRepository userRepository, StoreRepository storeRepository,
+            MovieRepository movieRepository,EventRepository eventRepository) {
         this.userRepository = userRepository;
         this.storeRepository = storeRepository;
+        this.movieRepository = movieRepository;
+        this.eventRepository = eventRepository;
     }
 
     // 총 회원 수
@@ -34,18 +43,42 @@ public class AdminController {
     @GetMapping("/users")
     public List<?> getAllUsers() {
         return userRepository.findAll();
-}
-
+    }
 
     // 총 스토어 상품 수
     @GetMapping("/store-count")
     public long getStoreCount() {
         return storeRepository.count();
     }
+
     // 스토어 목록
     @GetMapping("/store")
     public List<?> getAllstores() {
         return storeRepository.findAll();
-}
+    }
+
+    // 영화화 목록
+    @GetMapping("/movies")
+    public List<Movie> getAllMovies() {
+        return movieRepository.findAll(Sort.by(Sort.Direction.ASC, "rank")); // 랭킹순 정렬
+    }
+
+    // 총 영화 수
+    @GetMapping("/movie-count")
+    public long getMovieCount() {
+        return movieRepository.count();
+    }
+
+    // 이벤트 전체 리스트
+    @GetMapping("/events")
+    public List<Event> getAllEvents() {
+        return eventRepository.findAll();
+    }
+
+    // 이벤트 수 카운트
+    @GetMapping("/event-count")
+    public long getEventCount() {
+        return eventRepository.count();
+    }
 
 }
