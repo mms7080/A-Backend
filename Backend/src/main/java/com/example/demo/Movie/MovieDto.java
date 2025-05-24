21b84c5f0c435d5658d2220a435b5c9dd526e967
ParkChaeHun <pch3482@naver.com>
2025-05-25 05:04:58 +0900

    영화 DB 수정중

--- Backend/src/main/java/com/example/demo/Movie/MovieDto.java
+++ Backend/src/main/java/com/example/demo/Movie/MovieDto.java
@@ -10,28 +10,79 @@ import lombok.NoArgsConstructor;
 public class MovieDto {
     private Movie movie;
 
+    // Getter
     public String getTitle() { return movie.getTitle(); }
-    public void setTitle(String title) { movie.setTitle(title); }
+
+    public String getTitleEnglish() { return movie.getTitleEnglish(); }
 
     public String getRate() { return movie.getRate(); }
-    public void setRate(String rate) { movie.setRate(rate); }
 
     public String getReleaseDate() { return movie.getReleaseDate(); }
-    public void setReleaseDate(String releaseDate) { movie.setReleaseDate(releaseDate); }
+
+    public String getDescription() { return movie.getDescription(); }
+
+    public Integer getRunningTime() { return movie.getRunningTime(); }
+
+    public String getGenre() { return movie.getGenre(); }
+
+    public String getDirector() { return movie.getDirector(); }
+
+    public String getCast() { return movie.getCast(); }
+
+    public Double getScore() { return movie.getScore(); }
 
     public Long getLikeNumber() { return movie.getLikeNumber(); }
+
+    public String getPoster() { return movie.getPoster(); }
+
+    public String getWideImage() { return movie.getWideImage(); }
+
+    // public String[] getUrls() { return movie.getUrls(); }
+
+    public String getLabel() { return movie.getLabel(); }
+
+    public Double getReserveRate() { return movie.getReserveRate(); }
+
+    public Long getTotalView() { return movie.getTotalView(); }
+
+    public Integer getRank() { return movie.getRank(); }
+
+    // Setter
+    public void setId(Long id) { movie.setId(id); }
+
+    public void setTitle(String title) { movie.setTitle(title); }
+
+    public void setTitleEnglish(String titleEnglish) { movie.setTitleEnglish(titleEnglish); }
+
+    public void setRate(String rate) { movie.setRate(rate); }
+
+    public void setReleaseDate(String releaseDate) { movie.setReleaseDate(releaseDate); }
+
+    public void setDescription(String description) { movie.setDescription(description); }
+
+    public void setRunningTime(Integer runningTime) { movie.setRunningTime(runningTime); }
+
+    public void setGenre(String genre) { movie.setGenre(genre); }
+
+    public void setDirector(String director) { movie.setDirector(director); }
+
+    public void setCast(String cast) { movie.setCast(cast); }
+
+    public void setScore(Double score) { movie.setScore(score); }
+
     public void setLikeNumber(Long likeNumber) { movie.setLikeNumber(likeNumber); }
 
-    public String getImage() { return movie.getImage(); }
-    public void setImage(String image) { movie.setImage(image); }
+    public void setPoster(String poster) { movie.setPoster(poster); }
 
+    public void setWideImage(String wideImage) { movie.setWideImage(wideImage); }
 
-    public Integer getRank() {return movie.getRank();}
-    public void setRank(Integer rank) {movie.setRank(rank);}
-    public String getDescription() {return movie.getDescription();}
-    public void setDescription(String description) {movie.setDescription(description);}
-    public Double getScore() {return movie.getScore();}
-    public void setScore(Double score) {movie.setScore(score);}
-    public String getLabel() {return movie.getLabel();}
-    public void setLabel(String label) {movie.setLabel(label);}
+    // public void setUrls(String[] urls) { movie.setUrls(urls); }
+
+    public void setLabel(String label) { movie.setLabel(label); }
+
+    public void setReserveRate(Double reserveRate) { movie.setReserveRate(reserveRate); }
+
+    public void setTotalView(Long totalView) { movie.setTotalView(totalView); }
+
+    public void setRank(Integer rank) { movie.setRank(rank); }
 }
