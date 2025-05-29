824c323e0e0675bd714f0151504d94c0b3d24b38
kkj7584 <kkj758440@gmail.com>
2025-05-30 01:19:05 +0900

    리뷰 DB 제작중

--- Backend/src/main/java/com/example/demo/Review/ReviewController.java
+++ Backend/src/main/java/com/example/demo/Review/ReviewController.java
@@ -1,5 +1,32 @@
 package com.example.demo.Review;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+@RestController
+@RequestMapping("/review")
+
 public class ReviewController {
     
+    @Autowired
+    ReviewDao dao;
+
+    @GetMapping("/all")/* 모든든 리뷰들을 불러오기 */
+    public List<Review> getAllReviews() {
+        return null;
+        //return dao.findAll();
+    }
+    @GetMapping("/{id}")/* 영화 ID를 기반으로 리뷰들을 불러오기 */
+    public List<Review> getReviewsByMovieID(@PathVariable Long id) {
+        return null;
+        //return dao.findForMoviePage();
+    }
+
 }
--- Backend/src/main/java/com/example/demo/Review/ReviewDao.java
+++ Backend/src/main/java/com/example/demo/Review/ReviewDao.java
@@ -1,5 +1,8 @@
 package com.example.demo.Review;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ReviewDao {
     
 }
