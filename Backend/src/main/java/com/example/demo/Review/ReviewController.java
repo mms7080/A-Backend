a398aeeb168be920906dac511f642a2c8c46a490
kkj7584 <kkj758440@gmail.com>
2025-05-30 21:29:26 +0900

    리뷰 form 방식에서 fetch 방식으로 변경하느라 리뷰 컨트롤러도 변경

--- Backend/src/main/java/com/example/demo/Review/ReviewController.java
+++ Backend/src/main/java/com/example/demo/Review/ReviewController.java
@@ -4,25 +4,24 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.Annotations.Auth;
 import com.example.demo.User.User;
 
 @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 @RequestMapping("/review")
-
-@Controller
+@RestController
 public class ReviewController {
     
     @Autowired
@@ -32,20 +31,20 @@ public class ReviewController {
     String corsOrigin;
 
     @GetMapping("/all")/* 모든 리뷰들을 불러오기 */
-    @ResponseBody
     public List<Review> getAllReviews() {
         return dao.findAll();
     }
     @GetMapping("/{id}")/* 영화 ID를 기반으로 리뷰들을 불러오기 */
-    @ResponseBody
     public List<Review> getReviewsByMovieID(@PathVariable Long id) {
         return dao.findByMovieid(id);
     }
 
     @PostMapping("/logic/{movieid}")/* 리뷰 작성하기 */
-    public String writeReview(@Auth User user,@PathVariable Long movieid,
-    @RequestParam("content") String content,
-    @RequestParam("score") Integer score) {
+    public List<Review> writeReview(@Auth User user,@PathVariable Long movieid,
+    @RequestBody Map<String, String> data) {
+
+        String content = data.get("content");
+        Integer score = Integer.parseInt(data.get("score"));
 
         LocalDateTime now = LocalDateTime.now(); // 현재 시간
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"); // 포맷 지정
@@ -64,7 +63,7 @@ public class ReviewController {
             )
         );
 
-        return "redirect:"+corsOrigin+"/detail/"+movieid;
+        return dao.findByMovieid(movieid);
     }
 
 }
