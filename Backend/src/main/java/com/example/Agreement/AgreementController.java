355789acdce3642f87d97b0370788d25a94fcc61
kkj7584 <kkj758440@gmail.com>
2025-06-04 21:23:15 +0900

    이용약관 받아오는 컨트롤러 작성

---
+++ Backend/src/main/java/com/example/Agreement/AgreementController.java
@@ -0,0 +1,26 @@
+package com.example.Agreement;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+@RestController
+
+public class AgreementController {
+
+    @GetMapping("/agreement")
+    public List<String> getAgreement() throws IOException {
+        String[] agr = new String[5];
+        agr[0]=Files.readString(Path.of("./term1.txt"));
+        agr[1]=Files.readString(Path.of("./term2.txt"));
+        agr[2]=Files.readString(Path.of("./term3.txt"));
+        return List.of(agr[0],agr[1],agr[2]);
+    }
+
+}
