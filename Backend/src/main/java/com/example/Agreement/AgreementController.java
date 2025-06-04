51312c0089aa9e818b863295d15b331148dd0106
kkj7584 <kkj758440@gmail.com>
2025-06-04 21:46:56 +0900

    이용약관 코드 작성중(수정중)

--- Backend/src/main/java/com/example/Agreement/AgreementController.java
+++ Backend/src/main/java/com/example/Agreement/AgreementController.java
@@ -1,9 +1,6 @@
 package com.example.Agreement;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,16 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 @RestController
-
 public class AgreementController {
 
-    @GetMapping("/agreement")
-    public List<String> getAgreement() throws IOException {
-        String[] agr = new String[5];
-        agr[0]=Files.readString(Path.of("./term1.txt"));
-        agr[1]=Files.readString(Path.of("./term2.txt"));
-        agr[2]=Files.readString(Path.of("./term3.txt"));
-        return List.of(agr[0],agr[1],agr[2]);
+    @GetMapping("/agreement2")
+    //public List<String> getAgreement() throws IOException {
+    public String getAgreement() throws IOException {
+        // String[] agr = new String[5];
+        // agr[0]=Files.readString(Path.of("./term1.txt"));
+        // agr[1]=Files.readString(Path.of("./term2.txt"));
+        // agr[2]=Files.readString(Path.of("./term3.txt"));
+        // return List.of(agr[0],agr[1],agr[2]);
+        return "a";
     }
 
 }
