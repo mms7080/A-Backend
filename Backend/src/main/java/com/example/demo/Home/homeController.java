94f6ac7eacaf5f52717a97f556418afe87a77dd5
kkj7584 <kkj758440@gmail.com>
2025-06-06 19:35:32 +0900

    쓸모없는 html 파일 및 컨트롤러 파일들 전부 다 삭제

--- Backend/src/main/java/com/example/demo/Home/homeController.java
+++ Backend/src/main/java/com/example/demo/Home/homeController.java
@@ -1,7 +1,5 @@
 package com.example.demo.Home;
 
-import org.springframework.http.ResponseEntity;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -10,29 +8,14 @@ import java.nio.file.Paths;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.example.demo.Annotations.Auth;
-import com.example.demo.User.User;
-
 @Controller
 public class homeController{
-
-    @GetMapping("/home")
-    public String home(@Auth User user,Model model){
-        model.addAttribute("isLogin",user!=null);/* 현재 접속 중인 user 변수가 null인지 아닌지에 따라 isLogin 설정 */
-        if(user!=null){
-            model.addAttribute("user",user);/* 유저의 이름 또는 닉네임을 홈 화면에 표시하기 위해 값 설정  */
-            if(user.getPlatform()!= null)model.addAttribute("oauth2",true);/* OAuth2로 로그인한 사용자인지 판별해서 마이페이지 들어가지 못하게 하기 위해 설정 */
-            else model.addAttribute("oauth2",false);
-        }
-        return "home";
-    }
-
 
     @ResponseBody
     @GetMapping("/images/event/{id}")
