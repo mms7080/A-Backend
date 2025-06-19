b1ac396e31b3c1830ffda9da3b9e9eb1df53e7b4
kkj7584 <kkj758440@gmail.com>
2025-06-20 02:34:24 +0900

    유저 중복되지 않도록 DAOUser에서 테스트 유저 정보 입력

--- Backend/src/main/java/com/example/demo/User/DAOUser.java
+++ Backend/src/main/java/com/example/demo/User/DAOUser.java
@@ -1,10 +1,13 @@
 package com.example.demo.User;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import jakarta.annotation.PostConstruct;
 
 @Component
 public class DAOUser {
@@ -49,5 +52,70 @@ public class DAOUser {
     @Transactional
     public void Delete(User user) {
         userRepository.deleteById(user.getId());
+    }
+
+    // 초기 데이터 입력
+    @PostConstruct
+    @Transactional
+    public void testUsers() {
+        if(userRepository.count() > 0) return;
+        Insert(/* 테스트를 위한 유저 정보 입력 1 */
+            new User(
+                null,
+                "root",
+                new BCryptPasswordEncoder().encode("1234"),
+                "관리자",
+                "01011223344",
+                "honggildong@naver.com",
+                "1990-10-31",
+                "남성",
+                "21176",
+                "서울시 은평구",
+                "문새로72번길 13",
+                "ADMIN",
+                null,
+                "2025-05-16",
+                null
+            )
+        );
+        Insert(/* 테스트를 위한 유저 정보 입력 2 */
+            new User(
+                null,
+                "shin1234",
+                new BCryptPasswordEncoder().encode("1234"),
+                "신사임당",
+                "01099887766",
+                "shinsayimdang@yahoo.com",
+                "1998-07-05",
+                "여성",
+                "36569",
+                "경기도 남양주시",
+                "조화로14번길 26",
+                "USER",
+                null,
+                "2025-05-23",
+                null
+            )
+        );        
+
+        Insert(/* 테스트를 위한 유저 정보 입력 3 */
+            new User(
+                null,
+                "bum",
+                new BCryptPasswordEncoder().encode("1234"),
+                "박범수",
+                "01030600342",
+                "mms7080@naver.com",
+                "1990-10-31",
+                "남성",
+                "21176",
+                "서울시 은평구",
+                "문새로72번길 13",
+                "ADMIN",
+                null,
+                "2025-05-16",
+                null
+            )
+        ); 
     }
 }
