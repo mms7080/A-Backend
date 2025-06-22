25ac730adb2b79c16dd3b42184b2b614cfaeba73
kkj7584 <kkj758440@gmail.com>
2025-06-23 03:52:06 +0900

    import 문 자동수정

--- Backend/src/main/java/com/example/demo/Reservation/ReservationController.java
+++ Backend/src/main/java/com/example/demo/Reservation/ReservationController.java
@@ -6,11 +6,17 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.Payment.Payment;
 import com.example.demo.Payment.PaymentRepository;
